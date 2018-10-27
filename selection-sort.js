3a90745c523bce1d82bd0540fac4b845ff7038cf
Nikita Litvinenko <nik-el@mail.ru>
2018-10-27 16:00:33 +0300

    selection sort refac

--- selection-sort.js
+++ selection-sort.js
@@ -3,17 +3,16 @@ const testArray = [6, -100, 4, 999, -10000, 123];
 const selectionSort = (array) => {
   for(let i = 0; i < array.length; i++) {
     let minIndex = i;
-    let value = array[i];
 
     for (let j = i; j < array.length;  j++) {
-      if (value > array[j]) {
-        value = array[j];
+      if (array[i] > array[j]) {
         minIndex = j;
       }
     }
 
+    let minValue = array[minIndex];
     array[minIndex] = array[i];
-    array[i] = value;
+    array[i] = minValue;
   }
 
   return array;
